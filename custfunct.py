746305ce5d15669a09069ab21db1082a0245cef0
ryanabaldwin <ryanb24@byu.edu>
2025-03-01 16:04:56 -0700

    Update custfunct.py

--- custfunct.py
+++ custfunct.py
@@ -64,11 +64,13 @@
 #     print("Your team needs to practice!\n")
 
 """
-Garrett Safsten, Ryand Baldwin, Jack Mair, Tanner Crookston
+Garrett Safsten, Ryan Baldwin, Jack Mair, Tanner Crookston
 Section 003
 
 Description...
 """
+
+import random 
 
 # This is our introductory message to the user.
 print("This is where the intro will go when we have actually finished the code")
@@ -80,5 +82,99 @@ def first_name(fName) :
 print("Hey" + " " + first_name(name) + " " + "Welcome to the game!")
 
 
+""" 
+What else is needed: either here or above the name input function, there needs to be an 
+introductory message explaining the rules. At the bottom, there needs to be output that
+shows the results of the game between the two teams. We also need one more function.
 
+Whatever code makes the results at the end could probably be turned into a function.
+"""
+
+# This function asks the user to select a team and returns it
+def select_team():
+    team = input("\nPlease enter the team name here: ")
+    return team
+
+# This function generates random scores and determines if the home team won or lost
+def game() :
+    score1 = 0
+    score2 = 0
+    status = None
+
+    while score1 == score2 :
+        score1 = random.randrange(0, 99)
+        score2 = random.randrange(0, 99)
+
+    if score1 > score2 :
+        status = "W"
+    if score2 < score2 :
+        status = "L"
+
+    list_record = [score1, score2, status]
+
+    return list_record
+
+dictTeams = {
+    "Arizona": "Arizona",
+    "Arizona State": "Arizona State",
+    "Baylor": "Baylor",
+    "BYU": "BYU",
+    "UCF": "UCF",
+    "Cincinnati": "Cincinnati",
+    "Colorado": "Colorado",
+    "Houston": "Houston",
+    "Iowa State": "Iowa State",
+    "Kansas": "Kansas",
+    "Kansas State": "Kansas State",
+    "Oklahoma State": "Oklahoma State",
+    "TCU": "TCU",
+    "Texas Tech": "Texas Tech",
+    "Texas": "Texas",
+    "Utah": "Utah",
+    "West Virginia": "West Virginia"
+}
+
+def print_teams():
+    for iCount in dictTeams:
+        print(dictTeams[iCount])
+
+# This part displays teams and calls the function for the user to select a team
+print("Please choose a team from the following selections: \n")
+print_teams()
+
+sHomeTeam = select_team()
+
+# This takes the home team out of the dictionary
+# The while loop is so that the user cannont continue until they choose a team from the list
+bContinue = False
+while bContinue != True :
+    if sHomeTeam not in dictTeams :
+        print("Error: please select a team from the above list")
+        sHomeTeam = select_team()
+    else :
+        bContinue = True
+        dictTeams.pop(sHomeTeam)
+
+# Displays choices and asks user to select an opponent
+print("\nNow choose an opponent from the following selections: ")
+print_teams()
+
+sAwayTeam = select_team()
+
+# Makes sure the opponent team can be used and outputs the selections
+# The while loop is so that the user cannont continue until they choose a team from the list
+bContinue = False
+while bContinue != True :
+    if sAwayTeam not in dictTeams :
+        print("Error: please select a team from the above list")
+        sAwayTeam = select_team()
+    else :
+        bContinue = True
+
+print(f"\nYou have selected {sHomeTeam} and {sAwayTeam} as your team and opponent!")
+
+# Here's where the results are stored in a list (scores and W/L for the home team).
+# Whatever function is used to display results at the end could use the list below.
+
+lstResults = game()
 
